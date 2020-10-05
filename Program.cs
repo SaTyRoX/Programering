fdfdafcb2baea1b21d7e51ed927f72d332c44f49
SaTyRoX <Benjaminmargon@hotmail.com>
2020-10-05 16:14:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System; 
+  
+class program 
+{ 
+  
+    static void hanoi(int n, char f,char t, char v) 
+    { 
+        if (n == 0) { 
+            ;
+        } 
+        else {
+        hanoi(n-1, f, v, t); 
+        Console.WriteLine("Ryk disc " + f + " til " + t); 
+        hanoi(n-1, v, t, f); 
+        }
+    } 
+    public static void Main() 
+    { 
+        int n = 5;  
+
+        hanoi(n, 'A', 'C', 'B');  
+    } 
+} 
