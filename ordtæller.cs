d2693c4f034543c60330ddf6b077f31f2a9cd3f3
SaTyRoX <Benjaminmargon@hotmail.com>
2020-10-07 00:55:19 +0200

    Add files via upload

---
+++ ordtæller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ord_tæller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ord = 0;
+            string x;
+
+            Console.WriteLine("Indsæt tekst");
+
+            x = Console.ReadLine();
+
+            for(int i = 0; i < x.Length-1; i++) {
+                if(x[i] == ' ' && Char.IsLetter(x[i+1]) && (i>0)) {
+                    ord++;
+                }
+            }
+
+            ord++;
+
+            Console.WriteLine("Der er " + ord + " ord i teksten");
+
+
+        }
+    }
+
+}
